46fb77f980161383e14dd0f0c4f466590b687bde
Bia <fabianazs@gmail.com>
2019-05-27 22:32:22 -0300

    inclusao HBS

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,36 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 
-//minhaapp.com.br
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+
+
 app.get('',(req,res) => {
-    res.send('<h1>Hello minha app</h1')
+    res.render('index',{
+        title: 'Bem vindo ao sistema Curso nodejs',
+        author:' Bia'
+    })
 })
+
 //minhaapp.com.br/help
 app.get('/help',(req,res) => {
-    res.send('help page')
+    res.render('help',{
+        title: 'Ajuda'
+    })
 })
 //minhaapp.com.br/about
 app.get('/about',(req,res) => {
-    res.send('about page')
+    res.render('about',{
+        title: 'Sobre',
+        author:' Bia Zoroastro'
+    })
 })
 app.listen(3000,() => {
     console.log('server is up on port 3000')
